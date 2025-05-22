2bccb438128836503ef196c017ff1427343fde20
RufusWhelan <rufus.whelan@education.nsw.gov.au>
2025-05-22 17:29:24 +1000

    yes

--- Abstraction/Activity1/main.py
+++ Abstraction/Activity1/main.py
@@ -1,13 +1,18 @@
 from Shapey import *
 rectangle = Rectangle(5,10,8)
 circle = Circle(4)
+square = Square(5)
 
-print(rectangle.perimeter)
-print(rectangle.area)
-print(rectangle.volume)
+print(rectangle.perimeter())
+print(rectangle.area())
+print(rectangle.volume())
 
-print(circle.perimeter)
-print(circle.area)
-print(circle.volume)
+print(circle.perimeter())
+print(circle.area())
+print(circle.volume())
+
+print(square.perimeter())
+print(square.area())
+print(square.volume())
 
 
