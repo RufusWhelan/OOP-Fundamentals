667dcc5f467d2ea7efc773ab2b737a763f69f808
RufusWhelan <rufus.whelan@education.nsw.gov.au>
2025-05-28 10:09:02 +1000

    innit

---
+++ Inheritance/Activity1/animals.py
@@ -0,0 +1,10 @@
+class Animal():
+    def __init__(self, name, sound):
+        self._name = name
+        self._sound = sound
+
+    def get_name(self):
+        return self._name
+    def get_sound(self):
+        return self._name
+        
