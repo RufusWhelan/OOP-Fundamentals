b76f07f58b64f08480e2f378a032c92de56f1b4a
RufusWhelan <rufus.whelan@education.nsw.gov.au>
2025-05-30 14:34:23 +1000

    Inheritance

--- Inheritance/Activity1/animals.py
+++ Inheritance/Activity1/animals.py
@@ -1,10 +1,28 @@
-class Animal():
-    def __init__(self, name, sound):
+class Animal:
+    def __init__(self, name):
         self._name = name
+
+    def sound(self):
+        return "This animal makes a sound, but it's not defined."
+
+
+class Dog(Animal):
+    def __init__(self, name, sound):
+        super().__init__(name)
         self._sound = sound
 
-    def get_name(self):
-        return self._name
-    def get_sound(self):
-        return self._name
-        
+    def sound(self):
+        super().sound()
+        return f"The dog named {self._name} says: {self._sound}"
+
+
+class Cat(Animal):
+    def __init__(self, name, sound):
+        super().__init__(name)
+        self._sound = sound
+
+    def sound(self):
+        super().sound()
+        return f"The cat named {self._name} says: {self._sound}"
+
+    
---
+++ Inheritance/Activity1/main.py
@@ -0,0 +1,9 @@
+from animals import *
+monkey = Animal("Charlie")
+print(monkey.sound())
+
+charmander = Dog("Daniel","Bark")
+print(charmander.sound())
+
+squirtle = Cat("steve", "meow")
+print(squirtle.sound())
